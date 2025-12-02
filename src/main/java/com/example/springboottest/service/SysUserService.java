3dc78cc81fdf61c3bfad3bc807e7e34e985b0a64
谢沁桐 <2902125312@qq.com>
2025-12-02 14:40:13 +0800

    在SysUserService添加了一条import语句

--- src/main/java/com/example/springboottest/service/SysUserService.java
+++ src/main/java/com/example/springboottest/service/SysUserService.java
@@ -3,6 +3,7 @@ package com.example.springboottest.service;
 import com.example.springboottest.dao.SysUserMapper;
 import com.example.springboottest.entity.SysUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SysUserService {
